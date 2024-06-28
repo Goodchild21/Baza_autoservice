f7f16a6f060ce422a791a69e342c1878d0a959b1
Goodchild21 <Goodchild_21@mail.ru>
2024-06-29 02:17:41 +0300

    Baza_autoservice

---
+++ part_1/block_2/request_to_bd.sql
@@ -0,0 +1,20 @@
+INSERT INTO Сustumers (firstname, middlename, lastname, phone)
+    VALUES('Aleksey', 'Aleksandrovich', 'Batashev', +79534217689),
+        ('Ivan', 'Ivanovich', 'Ivanov', +79534217690),
+        ('Pedr', 'Pedrovich', 'Shpak', +79534217691);
+
+
+INSERT INTO Mechanics (firstname, middlename, lastname, specialization)
+    VALUES('Koliy', 'Kolich', 'engine_repair'),
+        ('Geka', 'Sipliy', 'gearbox repair'),
+        ('Michal', 'Michalich', 'all questions');
+
+    
+INSERT INTO Parts (parts, price)
+    VALUES('camshaft х 2', 34056.8),
+        ('transmission_oil х 4', 16400.6),
+        ('spark_plug х 4', 34056.8);
+
+
+INSERT INTO Orders (type_of_repair, total_price)
+    VALUES()
