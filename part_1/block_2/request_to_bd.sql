17b07c709cacc4ebe4163bbb100e6598cbe3ef56
Goodchild21 <Goodchild_21@mail.ru>
2024-06-30 11:24:14 +0300

    Update request_to_bd.sql

--- part_1/block_2/request_to_bd.sql
+++ part_1/block_2/request_to_bd.sql
@@ -1,20 +1,22 @@
 INSERT INTO Сustumers (firstname, middlename, lastname, phone)
-    VALUES('Aleksey', 'Aleksandrovich', 'Batashev', +79534217689),
-        ('Ivan', 'Ivanovich', 'Ivanov', +79534217690),
-        ('Pedr', 'Pedrovich', 'Shpak', +79534217691);
+    VALUES('Aleksey', 'Aleksandrovich', 'Batashev', 89534217689),
+        ('Ivan', 'Ivanovich', 'Ivanov', 89534217690),
+        ('Pedr', 'Pedrovich', 'Shpak', 89534217691);
 
 
-INSERT INTO Mechanics (firstname, middlename, lastname, specialization)
-    VALUES('Koliy', 'Kolich', 'engine_repair'),
-        ('Geka', 'Sipliy', 'gearbox repair'),
-        ('Michal', 'Michalich', 'all questions');
+INSERT INTO Mechanics (id, firstname, middlename, lastname, specialization)
+    VALUES(1, 'Koliy', 'Kolich', NULL, 'engine_repair'),
+        (2, 'Geka', 'Sipliy', NULL, 'gearbox_repair'),
+        (3, 'Michal', 'Michalich', NULL, 'all_questions');
 
     
-INSERT INTO Parts (parts, price)
-    VALUES('camshaft х 2', 34056.8),
-        ('transmission_oil х 4', 16400.6),
-        ('spark_plug х 4', 34056.8);
+INSERT INTO Parts (id, parts, price)
+    VALUES(1, 'camshaft х 2', 54000.8),
+        (2, 'transmission_oil х 4', 36400.6),
+        (3, 'spark_plug х 4', 16400.4);
 
 
-INSERT INTO Orders (type_of_repair, total_price)
-    VALUES()
+INSERT INTO Orders (id, type_of_repair, custumer_id, mechanic_id, parts_id, total_price, created_date)
+    VALUES(1, replacing camshafts, 1, 1, 1, 70000.8),
+        (2, replacing camshafts, 2, 2, 2, 50000.6),
+        (3, replacing camshafts, 3, 3, 3, 30000.4);
